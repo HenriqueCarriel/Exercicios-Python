9428afde587efdea4324dc4eeb914b56485bd77b
Henrique Carriel <75711179+HenriqueCarriel@users.noreply.github.com>
2024-03-20 00:29:24 -0300

    Add files via upload

---
+++ exc1.py
@@ -0,0 +1,2 @@
+nome = input("Digite seu nome: ")
+print("\nÉ um prazer te conhecer, \033[4m\033[1m{}!".format(nome))
---
+++ exc2.py
@@ -0,0 +1,7 @@
+n1 = int(input("Digite um valor: "))
+n2 = int(input("Digite outro valor: "))
+soma = n1 + n2
+print("\nA soma entre {} e {} é igual a \033[1m\033[4m{}".format(n1, n2, soma))
+
+# \033[1m - Bold Text Code #
+# \033[4m - Underline Text Code #
